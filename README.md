6ad7aa8a59e28422aa7230b2e0a3de8bd58369a7
Katrina Nguyen <katrinan@usc.edu>
2021-01-27 14:27:16 -0800

    updating final files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# javascript-challenge
+## Background
+
+WAKE UP SHEEPLE! The extra-terrestrial menace has come to Earth and we here at `ALIENS-R-REAL` have collected all of the eye-witness reports we could to prove it! All we need to do now is put this information online for the world to see and then the matter will finally be put to rest.
+
+There is just one tiny problem though... our collection is too large to search through manually. Even our most dedicated followers are complaining that they are having trouble locating specific reports in this mess.
+
+## Objective
+
+The objective of this challenge was to write code that will create a table dynamically based upon a dataset. 
+
+## Result
+
+The app.js file will allow users to filter the table data by date only using pure JavaScript, HTML, and CSS, and D3.js on our web pages.
+
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,49 +1,58 @@
 // from data.js
+const tableData = data;
 
-// var data = [{
-//   datetime: "1/1/2010",
-//   city: "benton",
-//   state: "ar",
-//   country: "us",
-//   shape: "circle",
-//   durationMinutes: "5 mins.",
-//   comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
-// },
+// select button
+const button = d3.select("#filter-btn");
 
-let tableData = data;
+// select form
+const form = d3.select("form");
+
+// select tbody
 let tbody = d3.select('tbody');
 
+// append tbody and have table populate
 data.forEach((ufoSighting) => {
-  var row = tbody.append("tr");
+  const row = tbody.append("tr");
   Object.entries(ufoSighting).forEach(([key, value]) => {
-    var cell = row.append("td");
+    const cell = row.append("td");
     cell.text(value);
   });
 });
 
-// let row = tbody.append('tr');
-// let li = row.append('td').text(data[0].datetime);
-// let li2 = row.append('td').text(data[0].city);
-// let li3 = row.append("td").text(data[0].state);
-// let li4 = row.append("td").text(data[0].country);
-// let li5 = row.append("td").text(data[0].shape);
-// let li6 = row.append("td").text(data[0].durationMinutes);
-// let li7 = row.append("td").text(data[0].comments);
-// let row1 = tbody.append('tr');
-// let a = row1.append('td').text(data[1].datetime);
-// let b = row1.append('td').text(data[1].city);
-// let c = row1.append("td").text(data[1].state);
-// let d = row1.append("td").text(data[1].country);
-// let e = row1.append("td").text(data[1].shape);
-// let f = row1.append("td").text(data[1].durationMinutes);
-// let g = row1.append("td").text(data[1].comments);
+// create event handlers
+button.on("click", runEnter);
+form.on("submit",runEnter);
 
-// var li = d3.select("tbody").append("tr");
-// var li1 = d3.select("tbody").append("td").text(data[0].datetime);
-// var li2 = d3.select("tbody").append("td").text(data[0].city);
-// var li3 = d3.select("tbody").append("td").text(data[0].state);
-// var li4 = d3.select("tbody").append("td").text(data[0].country);
-// var li5 = d3.select("tbody").append("td").text(data[0].shape);
-// var li6 = d3.select("tbody").append("td").text(data[0].durationMinutes);
-// var li7 = d3.select("tbody").append("td").text(data[0].comments);
-// YOUR CODE HERE!
+// complete the event handler function for the form
+function runEnter() {
+
+    // prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // select the input element and get the raw HTML node
+    const inputElement = d3.select("#datetime");
+
+    // get the value property of teh input element
+    const inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    const filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+
+    let tbody = d3.select("tbody");
+
+    tbody.text("");
+
+    // append to the list
+
+    filteredData.forEach((ufoSighting) => {
+      const row = tbody.append("tr");
+      Object.entries(ufoSighting).forEach(([key, value]) => {
+        const cell = row.append("td");
+        cell.text(value);
+      });
+    });
+};
+
